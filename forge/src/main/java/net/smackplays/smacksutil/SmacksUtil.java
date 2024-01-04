be1c3a8b8808b9b2761a9691f53a0e4b63131815
smacksplays <p.enzinger5555@gmail.com>
2024-01-04 21:55:09 +0100

    forge backpack implementation

--- forge/src/main/java/net/smackplays/smacksutil/SmacksUtil.java
+++ forge/src/main/java/net/smackplays/smacksutil/SmacksUtil.java
@@ -1,19 +1,28 @@
 package net.smackplays.smacksutil;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.smackplays.smacksutil.backpacks.BackpackItem;
+import net.smackplays.smacksutil.backpacks.LargeBackpackContainerMenu;
+import net.smackplays.smacksutil.backpacks.LargeBackpackItem;
+import net.smackplays.smacksutil.backpacks.LargeBackpackScreen;
 import net.smackplays.smacksutil.config.ClothConfigForge;
 import net.smackplays.smacksutil.veinminer.Miner;
 import org.slf4j.Logger;
@@ -22,6 +31,12 @@ import org.slf4j.Logger;
 @Mod(Constants.MOD_ID)
 public class SmacksUtil {
     public static final String MOD_ID = Constants.MOD_ID;
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+    public static final RegistryObject<Item> BACKPACK_ITEM = ITEMS.register("backpack_item", BackpackItem::new);
+    public static final RegistryObject<Item> LARGE_BACKPACK_ITEM = ITEMS.register("large_backpack_item", LargeBackpackItem::new);
+    public static final DeferredRegister<MenuType<?>> SCREEN = DeferredRegister.create(ForgeRegistries.MENU_TYPES, MOD_ID);
+    public static final RegistryObject<MenuType<LargeBackpackContainerMenu>> GENERIC_13X9 =
+            SCREEN.register("large_backpack_screen", () -> IForgeMenuType.create(LargeBackpackContainerMenu::createGeneric13x9));
     private static final Logger LOGGER = LogUtils.getLogger();
     public static Miner veinMiner;
 
@@ -38,6 +53,10 @@ public class SmacksUtil {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
+
+        SCREEN.register(modEventBus);
+
+        ITEMS.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
 
@@ -65,6 +84,7 @@ public class SmacksUtil {
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
+            MenuScreens.register(SmacksUtil.GENERIC_13X9.get(), LargeBackpackScreen::new);
         }
     }
 }
